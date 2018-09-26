fde887c9319231064afc3447d1c2caaa5882ea0d
Unknown <wndtanaka@gmail.com>
2018-09-26 23:03:36 +1000

    Created a table for yearly average.

--- Project.py
+++ Project.py
@@ -22,23 +22,31 @@ middle_class_countries = {}
 low_class_countries = {}
 happiness_rank = {}
 
+avg_data_per_year = {}
+
 # Average SES
 all_avg_ses = 0
 low_avg_ses = 0
 middle_avg_ses = 0
 high_avg_ses = 0
+yearly_avg_ses = 0
 
 # Average GDP
 all_avg_gdp = 0
 low_avg_gdp = 0
 middle_avg_gdp = 0
 high_avg_gdp = 0
+yearly_avg_gdp = 0
 
 # Average Years of education
 all_avg_edu = 0
 low_avg_edu = 0
 middle_avg_edu = 0
 high_avg_edu = 0
+yearly_avg_edu = 0
+
+
+# Misc
 
 
 # FUNCTIONS
@@ -76,6 +84,15 @@ def countries_table(countries_list):
     print()
 
 
+def yearly_avg_table(yearly_list):
+    print("{:<5} {:<9} {:<12} {:<8}".format('Year', 'SES', 'GDP', 'Education Average'))
+    for year in yearly_list:
+        print("{:<5} {:<9,.2f} {:<12,.2f} {:<8,.2f}".format(year, yearly_list[year]['SES'],
+                                                              yearly_list[year]['GDPPC'],
+                                                              yearly_list[year]['Edu']))
+    print()
+
+
 ''' How to do in Pandas
 # for country in country_list:
 #     if country not in countries_with_class:
@@ -93,7 +110,7 @@ for row in open("Data/GlobalSES.csv"):
         values = row.split(",")
         # GET all the values by the column
         country = values[2]
-        year = values[3]
+        year = int(values[3])
         ses = float(values[4])
         class_level = values[5]
         gdppc = float(values[6])
@@ -101,21 +118,28 @@ for row in open("Data/GlobalSES.csv"):
         # CLEAN -- Change C?te d'Ivoire to Ivory Coast
         if country == "C?te d'Ivoire":
             country = "Ivory Coast"
-        if country not in all_countries_list and year == '2010':
+        if country not in all_countries_list and year == 2010:
             all_countries_list[country] = {"SES": ses, "GDPPC": gdppc, "Edu": years_education}
         # CLEAN -- Change the description to make it more neat and store the require values in a dictionary
         if class_level == "High(core)":
             class_level = "High"
-            if country not in high_class_countries and year == '2010':
+            if country not in high_class_countries and year == 2010:
                 high_class_countries[country] = {"SES": ses, "GDPPC": gdppc, "Edu": years_education}
         elif class_level == "Middle(semi-per)":
             class_level = "Middle"
-            if country not in middle_class_countries and year == '2010':
+            if country not in middle_class_countries and year == 2010:
                 middle_class_countries[country] = {"SES": ses, "GDPPC": gdppc, "Edu": years_education}
         elif class_level == "Low(periphery)":
             class_level = "Low"
-            if country not in low_class_countries and year == '2010':
+            if country not in low_class_countries and year == 2010:
                 low_class_countries[country] = {"SES": ses, "GDPPC": gdppc, "Edu": years_education}
+        while year >= 1880 and year <= 2010:
+            if year not in avg_data_per_year:
+                avg_data_per_year[year] = {'SES': 0, 'GDPPC': 0, 'Edu': 0}
+            avg_data_per_year[year]['SES'] += ses
+            avg_data_per_year[year]['GDPPC'] += gdppc
+            avg_data_per_year[year]['Edu'] += years_education
+            break
 
 # DATA CLEANING and data wrangling in Python for Happiness Level
 is_first_line = True
@@ -127,134 +151,156 @@ for row in open("Data/Happiness_2017.csv"):
         # GET all the values by the column
         country = values[0]
         rank = values[1]
-        life_expectancy = values[7]
-        freedom = values[8]
-        generosity = values[9]
-        govt_trust = values[10]
+        happiness_score = float(values[2])
+        gdp = float(values[5])
+        life_expectancy = float(values[7])
+        freedom = float(values[8])
+        generosity = float(values[9])
+        govt_trust = float(values[10])
         if country not in happiness_rank:
-            happiness_rank[country] = {"Rank": rank, "LifeExp": life_expectancy, "Freedom": freedom,
-                                       "Generosity": generosity, "GovtTrust": govt_trust}
-for country in happiness_rank:
-    print(country, happiness_rank[country])
+            happiness_rank[country] = {"Rank": rank, 'Happiness': round(happiness_score, 2), 'GDP': round(gdp, 2),
+                                       "LifeExp": round(life_expectancy, 2), "Freedom": round(freedom, 2),
+                                       "Generosity": round(generosity, 2), "GovtTrust": round(govt_trust, 2)}
+
+# for country in happiness_rank:
+#    print(country, happiness_rank[country])
+
+# Scatter plot from Happiness dataset, comparing Happiness score v Life expectancy
+# happiness_df.plot.scatter(x='Happiness.Score', y='Health..Life.Expectancy.')
+# plt.suptitle('Test Title')
+# plt.xlabel('Happiness')
+# plt.ylabel('Life Expectancy')
+# plt.show()
+
+# Scatter plot from Happiness dataset, comparing Happiness score v GDP
+# happiness_df.plot.scatter(x='Happiness.Score', y='Economy..GDP.per.Capita.')
+# plt.suptitle('Test Title')
+# plt.xlabel('Happiness')
+# plt.ylabel('GDP per Capita')
+# plt.show()
+
 # global_SES.plot.scatter(x='gdppc', y='yrseduc', marker='^')
 # plt.show()
 
-# while not quit_application:
-#     welcome = input("Please type your selection number\n"
-#                     "1. Print Table for countries\n"
-#                     "2. SES\n"
-#                     "3. GDP per capita\n"
-#                     "4. Years of education\n"
-#                     "0. Quit program\n"
-#                     "> ")
-#     os.system('cls')
-#     # PRINT the data in a table like structure
-#     while welcome == '1':
-#         command = input("Please type your selection number country table\n"
-#                         "1. All countries\n"
-#                         "2. Low class countries\n"
-#                         "3. Middle class countries\n"
-#                         "4. High class countries\n"
-#                         "0. Back to main menu\n"
-#                         "> ")
-#         os.system('cls')
-#         if command == '1':
-#             countries_table(all_countries_list)
-#         elif command == '2':
-#             countries_table(low_class_countries)
-#         elif command == '3':
-#             countries_table(middle_class_countries)
-#         elif command == '4':
-#             countries_table(high_class_countries)
-#         elif command == '0':
-#             break
-#         else:
-#             print("Your input is invalid")
-#     # PRINT Average SES
-#     while welcome == '2':
-#         command = input("Please type your selection number for SES stats\n"
-#                         "1. Average all countries\n"
-#                         "2. Average low class countries\n"
-#                         "3. Average middle class countries\n"
-#                         "4. Average high class countries\n"
-#                         "0. Back to main menu\n"
-#                         "> ")
-#         os.system('cls')
-#         if command == '1':
-#             cls = 'all'
-#             count_avg_ses(all_countries_list, all_avg_ses, cls)
-#         elif command == '2':
-#             cls = 'low'
-#             count_avg_ses(low_class_countries, low_avg_ses, cls)
-#         elif command == '3':
-#             cls = 'middle'
-#             count_avg_ses(middle_class_countries, middle_avg_ses, cls)
-#         elif command == '4':
-#             cls = 'high'
-#             count_avg_ses(high_class_countries, high_avg_ses, cls)
-#         elif command == '0':
-#             break
-#         else:
-#             print("Your input is invalid")
-#     # PRINT Average GDP per capita
-#     while welcome == '3':
-#         command = input("Please type your selection number for GDP stats\n"
-#                         "1. Average all countries\n"
-#                         "2. Average low class countries\n"
-#                         "3. Average middle class countries\n"
-#                         "4. Average high class countries\n"
-#                         "0. Back to main menu\n"
-#                         "> ")
-#         os.system('cls')
-#         if command == '1':
-#             cls = 'all'
-#             count_avg_gdp(all_countries_list, all_avg_gdp, cls)
-#         elif command == '2':
-#             cls = 'low'
-#             count_avg_gdp(low_class_countries, low_avg_gdp, cls)
-#         elif command == '3':
-#             cls = 'middle'
-#             count_avg_gdp(middle_class_countries, middle_avg_gdp, cls)
-#         elif command == '4':
-#             cls = 'high'
-#             count_avg_gdp(high_class_countries, high_avg_gdp, cls)
-#         elif command == '0':
-#             break
-#         else:
-#             print("Your input is invalid")
-#     # PRINT Average Education years
-#     while welcome == '4':
-#         command = input("Please type your selection number for Years of Education stats\n"
-#                         "1. Average all countries\n"
-#                         "2. Average low class countries\n"
-#                         "3. Average middle class countries\n"
-#                         "4. Average high class countries\n"
-#                         "0. Back to main menu\n"
-#                         "> ")
-#         os.system('cls')
-#         if command == '1':
-#             cls = 'all'
-#             count_avg_edu(all_countries_list, all_avg_edu, cls)
-#         elif command == '2':
-#             cls = 'low'
-#             count_avg_edu(low_class_countries, low_avg_edu, cls)
-#         elif command == '3':
-#             cls = 'middle'
-#             count_avg_edu(middle_class_countries, middle_avg_edu, cls)
-#         elif command == '4':
-#             cls = 'high'
-#             count_avg_edu(high_class_countries, high_avg_edu, cls)
-#         elif command == '0':
-#             break
-#         else:
-#             print("Your input is invalid")
-#     # Invalid input
-#     if welcome != '1' and welcome != '2' and welcome != '3' and welcome != '4' and welcome != '5' and welcome != '0':
-#         os.system('cls')
-#         print("Your input is invalid")
-#     # QUIT the program
-#     if welcome == '0':
-#         os.system('cls')
-#         print("Thank you for using this program")
-#         quit_application = True
-#         break
+while not quit_application:
+    welcome = input("Please type your selection number\n"
+                    "1. Print Table for countries\n"
+                    "2. SES\n"
+                    "3. GDP per capita\n"
+                    "4. Years of education\n"
+                    "0. Quit program\n"
+                    "> ")
+    os.system('cls')
+    # PRINT the data in a table like structure
+    while welcome == '1':
+        command = input("Please type your selection number\n"
+                        "1. All countries stats\n"
+                        "2. Low class countries stats\n"
+                        "3. Middle class countries stats\n"
+                        "4. High class countries stats\n"
+                        "5. Yearly stats for all countries\n"
+                        "0. Back to main menu\n"
+                        "> ")
+        os.system('cls')
+        if command == '1':
+            countries_table(all_countries_list)
+        elif command == '2':
+            countries_table(low_class_countries)
+        elif command == '3':
+            countries_table(middle_class_countries)
+        elif command == '4':
+            countries_table(high_class_countries)
+        elif command == '5':
+            yearly_avg_table(avg_data_per_year)
+        elif command == '0':
+            break
+        else:
+            print("Your input is invalid")
+    # PRINT Average SES
+    while welcome == '2':
+        command = input("Please type your selection number for SES stats\n"
+                        "1. Average all countries\n"
+                        "2. Average low class countries\n"
+                        "3. Average middle class countries\n"
+                        "4. Average high class countries\n"
+                        "0. Back to main menu\n"
+                        "> ")
+        os.system('cls')
+        if command == '1':
+            cls = 'all'
+            count_avg_ses(all_countries_list, all_avg_ses, cls)
+        elif command == '2':
+            cls = 'low'
+            count_avg_ses(low_class_countries, low_avg_ses, cls)
+        elif command == '3':
+            cls = 'middle'
+            count_avg_ses(middle_class_countries, middle_avg_ses, cls)
+        elif command == '4':
+            cls = 'high'
+            count_avg_ses(high_class_countries, high_avg_ses, cls)
+        elif command == '0':
+            break
+        else:
+            print("Your input is invalid")
+    # PRINT Average GDP per capita
+    while welcome == '3':
+        command = input("Please type your selection number for GDP stats\n"
+                        "1. Average all countries\n"
+                        "2. Average low class countries\n"
+                        "3. Average middle class countries\n"
+                        "4. Average high class countries\n"
+                        "0. Back to main menu\n"
+                        "> ")
+        os.system('cls')
+        if command == '1':
+            cls = 'all'
+            count_avg_gdp(all_countries_list, all_avg_gdp, cls)
+        elif command == '2':
+            cls = 'low'
+            count_avg_gdp(low_class_countries, low_avg_gdp, cls)
+        elif command == '3':
+            cls = 'middle'
+            count_avg_gdp(middle_class_countries, middle_avg_gdp, cls)
+        elif command == '4':
+            cls = 'high'
+            count_avg_gdp(high_class_countries, high_avg_gdp, cls)
+        elif command == '0':
+            break
+        else:
+            print("Your input is invalid")
+    # PRINT Average Education years
+    while welcome == '4':
+        command = input("Please type your selection number for Years of Education stats\n"
+                        "1. Average all countries\n"
+                        "2. Average low class countries\n"
+                        "3. Average middle class countries\n"
+                        "4. Average high class countries\n"
+                        "0. Back to main menu\n"
+                        "> ")
+        os.system('cls')
+        if command == '1':
+            cls = 'all'
+            count_avg_edu(all_countries_list, all_avg_edu, cls)
+        elif command == '2':
+            cls = 'low'
+            count_avg_edu(low_class_countries, low_avg_edu, cls)
+        elif command == '3':
+            cls = 'middle'
+            count_avg_edu(middle_class_countries, middle_avg_edu, cls)
+        elif command == '4':
+            cls = 'high'
+            count_avg_edu(high_class_countries, high_avg_edu, cls)
+        elif command == '0':
+            break
+        else:
+            print("Your input is invalid")
+    # Invalid input
+    if welcome != '1' and welcome != '2' and welcome != '3' and welcome != '4' and welcome != '5' and welcome != '0':
+        os.system('cls')
+        print("Your input is invalid")
+    # QUIT the program
+    if welcome == '0':
+        os.system('cls')
+        print("Thank you for using this program")
+        quit_application = True
+        break
